82f018b1007f926a055a1311a5937640b952ebe5
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2023-12-26 23:36:19 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1812A | Are You a Robot?/200253807.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using ld = long double;
+using ll = long long int;
+using namespace __gnu_pbds;
+ 
+#pragma GCC optimize("-Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx2,tune=native")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-funroll-all-loops,-fpeel-loops,-funswitch-loops")
+ 
+#define sp ' '
+#define nl '\n'
+#define F first
+#define S second
+#define oo 0x3f3f3f3f
+#define OO 0x3f3f3f3f3f3f3f3f
+#define pb push_back
+#define eb emplace_back
+#define pii pair<int, int>
+#define sz(s) (int)(s).size()
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define fs(n) fixed<<setprecision(n)
+#define mem(x, y, SZ) memset(x, y, (SZ) * sizeof(x[0]))
+#define test int test_case = 1; while(test_case--)
+#define tests int test_cases; cin >> test_cases; for(int tc=1; tc<=test_cases; tc++)
+#define RamadanKareem ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ordered_set tree<int , null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+const ll N = 4e6 + 5, MOD = 1e9 + 7;
+ 
+void y3ni_mht3dee4() {
+  cout << "security\n";
+}
+ 
+signed main() {
+  RamadanKareem
+  y3ni_mht3dee4();
+}
