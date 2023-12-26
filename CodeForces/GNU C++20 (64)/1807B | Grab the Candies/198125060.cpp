1774a3dca8e8ab63cc8a96bfb0e5065f3a328a84
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2023-12-26 23:37:37 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1807B | Grab the Candies/198125060.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace std;
+using ld = long double;
+using ll = long long int;
+using namespace __gnu_pbds;
+ 
+#define sp ' '
+#define nl '\n'
+#define F first
+#define S second
+#define oo INT_MAX
+#define OO LLONG_MAX
+#define pb push_back
+#define eb emplace_back
+#define pii pair<int, int>
+#define sz(s) (int)(s).size()
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define mem(x, y, SZ) memset(x, y, (SZ) * sizeof(x[0]))
+#define tests int test_cases; cin >> test_cases; for(int tc=1; tc<=test_cases; tc++)
+#define RamadanKareem ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ordered_set tree<int , null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+const ll N = 1e5+5, MOD = 1e9 + 7;
+ 
+void y3ni_mht3dee4(){
+  int n;
+  cin >> n;
+  ll e =0 , o = 0, x;
+  for(int i=1; i<=n; i++){
+    cin >> x;
+    if(x&1) o+=x;
+    else e+=x;
+  }
+  if(e > o)cout << "Yes\n";
+  else cout << "No\n";
+}
+ 
+signed main() {
+  RamadanKareem
+  tests
+    y3ni_mht3dee4();
+  return 0;
+}
