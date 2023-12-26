b0a27c749c7781e9c0ba129b6452f898f84802e6
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2023-12-26 23:05:22 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc061_d/33889087.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define ll long long int
+#define pii pair<ll,ll>
+#define mp make_pair
+#define pb push_back
+#define oo 0x3f3f3f3f3f3f3f3f
+#define N  1005
+
+ll dist[N], n, m, ans;
+bool posCycle = false;
+vector<pair<int, pii>>edges;
+
+void Bellman_Ford(int src){
+	for(int i = 0; i <= n; i++){
+		dist[i] = -oo;
+	}
+
+	dist[src] = 0;
+	posCycle = false;
+	for(ll i=1; i<=2 * n; i++){
+		for(ll j=0; j<edges.size(); j++){
+			ll w = edges[j].F;
+			ll u = edges[j].S.F;
+			ll v = edges[j].S.S;
+			if(dist[u] != -oo and dist[u] + w > dist[v]){
+				if(i == 2 * n and v == n)posCycle = true;
+				dist[v] = dist[u] + w;
+			}
+		}
+	}
+}
+
+int main(){
+	cin >> n >> m;
+	for(int i=1; i<=m; i++){
+		int a, b, c;
+		cin >> a >> b >> c;
+		edges.pb({c, {a, b}});
+	}
+
+	Bellman_Ford(1);
+	if(posCycle)cout<<"inf";
+	else cout<<dist[n]<<'\n';
+}
