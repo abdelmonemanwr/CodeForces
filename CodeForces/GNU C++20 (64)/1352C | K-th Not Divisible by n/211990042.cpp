dcd63a1318302252144be3473018b0edf557db26
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2023-12-26 23:30:35 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1352C | K-th Not Divisible by n/211990042.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ld = long double;
+using ll = long long int;
+ 
+#define nl '\n'
+#define sp ' '
+#define F first
+#define S second
+#define EPS 1e-6
+#define pb push_back
+#define mkp make_pair
+#define r(x) return x
+#define rv return void
+#define em emplace_back
+#define sz(s) (int)(s.size())
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define Tests int test_cases;cin>>test_cases; for(int tc=1; tc<=test_cases; tc++)
+#define Y3niMht3dee4 ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+const ld PI = acos(-1);
+const int oo = 0x3f3f3f3f;
+const ll OO = 0x3f3f3f3f3f3f3f3f;
+const int N = 2e5 + 5, M = 1e6 + 5, MOD = 1e9 + 7;
+ 
+void Y3niMht3deee4() {
+    int n, k;
+    cin >> n >> k;
+    ll ans = k, prv = 0;
+    int iterations = 0;
+    while (true) {
+        ll divisible = ans / n - prv / n;
+        if (!divisible)break;
+        iterations++;
+        prv = ans;
+        ans += divisible;
+    }
+    // cout << "iterations = " << iterations << nl;
+    // complexity : O(log(number, base)) --> O(log (k, n))
+    if (!(ans % n))ans++;
+    cout << ans << nl;
+}
+ 
+signed main() {
+    Y3niMht3dee4
+    Tests Y3niMht3deee4();
+}
