fd3097f57173f260a74502000aeb38d73729153a
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2023-12-26 23:32:56 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1840C | Ski Resort/209009926.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long
+#define ld long double
+ 
+#define nl '\n'
+#define sp ' '
+#define F first
+#define S second
+#define EPS 1e-4
+#define pb push_back
+#define mkp make_pair
+#define rz return 0;
+#define rv return void
+#define em emplace_back
+#define Sz(s) (int)(s.size())
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define Tests int test_cases;cin>>test_cases; for(int tc=1; tc<=test_cases; tc++)
+#define IOS ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+ll M = 1;
+const ld PI = acos(-1);
+const ll N = 1e6 + 5, MOD = 1e9 + 7, oo = 2e9, OO = 2e18;
+ 
+void y3ni_mht3dee4() {
+  ll n, k, q, x;
+  cin >> n >> k >> q;
+  ll ans = 0, days = 0;
+  for(int i=0; i<n; i++){
+    cin >> x;
+    if(x<=q){
+      days++;
+    } else {
+      if(days>=k){
+        ans += ((days-k)*(days-k)+3*(days-k)+2)/2;
+      }
+      days=0;
+    }
+  }
+  if(days>=k){
+    ans += ((days-k)*(days-k)+3*(days-k)+2)/2;
+  }
+  cout << ans <<nl;
+}
+ 
+signed main()
+{
+  IOS;
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#else
+#endif
+  Tests y3ni_mht3dee4();
+}
