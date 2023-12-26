e2e252d4a64d991f17f26ee484a90726b22c071c
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2023-12-26 23:29:36 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1855B | Longest Divisors Interval/216431819.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define nl '\n'
+#define sp ' '
+#define em emplace_back
+#define sz(s) (int)(s.size())
+#define IOS ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+ 
+const long long OO = 0x3f3f3f3f3f3f3f3f;
+const int N = 1e4 + 3, M = 1e6 + 7, MOD = 1e9 + 7, oo = 0x3f3f3f3f;
+ 
+bool comp(pair<int, int> a, pair<int, int> b) {
+    return a.second < b.second;
+}
+// test tle || wa
+void Y3ni_Mht3dee4() {
+    long long n;
+    cin >> n;
+    int ans = 0, mx = 0;
+    for (int i = 1; i <= 1000; i++) {
+        if (n % i) {
+            ans = max(ans, mx);
+            mx = 0;
+        } else {
+            mx++;
+        }
+    }
+    cout << ans << nl;
+}
+ 
+signed main() {
+    IOS
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        Y3ni_Mht3dee4();
+    }
+}
