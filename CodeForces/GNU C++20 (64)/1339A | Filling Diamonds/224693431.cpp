691f1c251e1235d4c1c339a48a5e6adcd4f9d2d5
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2023-12-26 23:28:54 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1339A | Filling Diamonds/224693431.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long int;
+const int N = 1e6+5;
+ 
+void easy(){
+ int n;
+ cin >> n;
+ cout << n << '\n';
+}
+ 
+ 
+int main() {
+ ios_base::sync_with_stdio(0), cout.tie(0), cin.tie(0);
+ int t = 1;
+ cin >> t;
+ while (t--) {
+  easy();
+ }
+ return 0;
+}
